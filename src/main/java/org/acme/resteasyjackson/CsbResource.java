8241218e44217eb50c24a0b57fef718553649381
Felipe Yanaga <felipe@yanaga.com.br>
2021-05-12 16:54:51 -0400

    Add transactional to get

--- src/main/java/org/acme/resteasyjackson/CsbResource.java
+++ src/main/java/org/acme/resteasyjackson/CsbResource.java
@@ -14,6 +14,7 @@ import java.util.List;
 public class CsbResource {
 
     @GET
+    @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     public List<Dish> getItems() throws IOException {
         return Dish.listAll();
